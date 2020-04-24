2e0097a5531718238dd5cfd9b1200858afd6dc49
Alexander Amini <amini@mit.edu>
2020-04-24 11:03:35 -0400

    package structure and gitignore

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # opt-uncertainty
 6.881 Final Project
+
+## Installation
+
+```
+git clone git@github.com:roshni714/opt-uncertainty.git
+cd opt-uncertainty
+pip install -e .
+```
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='edl',
+      version='0.1',
+      description='Final project for 6.881',
+      url='https://github.com/roshni714/opt-uncertainty/',
+      author='Roshni Sahoo and Alexander Amini',
+      license='MIT',
+      packages=['edl'])
