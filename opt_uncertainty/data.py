small changes and fixed rotation bug

opt_uncertainty/data.py
@@ -28,7 +28,7 @@ def get_dataset(dataset):
 def apply_corruption_to_dataset(imgs, corruption_type, corruption_level):
 
     if corruption_type == "rotation":
-        angles = [corruption_level * math.pi/180. for i in range(imgs.shape[0])]
+        angles = [corruption_level for i in range(imgs.shape[0])]
         corr_imgs = rotate_imgs(imgs, angles)
     elif corruption_type == "brightness":
         corr_imgs = tf.image.adjust_brightness(imgs, corruption_level)

test_calibration.py
@@ -13,17 +13,18 @@ checkpoint_path = max(glob.glob('save/MNIST/*.h5'), key=os.path.getmtime) # get 
 model = tf.keras.models.load_model(checkpoint_path, custom_objects={
     'DenseDirichlet':edl.layers.DenseDirichlet,
     'DenseSigmoid':edl.layers.DenseSigmoid,
-})
+}, compile=False)
+
 
 # Specify dataset, type of corruption, and corruption range
 dataset = "MNIST"
 (x_train, y_train), (x_test, y_test) = opt_uncertainty.data.get_dataset(dataset)
 corruption_type = "rotation"
-corruption_range = [-20, 20]
+corruption_range = [-360, 360]
 #corruption_type = "brightness"
 #corruption_range = [0.001, 2]
 
-corruption_amounts = np.linspace(corruption_range[0], corruption_range[1], 20)
+corruption_amounts = np.linspace(corruption_range[0], corruption_range[1], 40)
 
 # Get the mean uncertainty+accuracy  at a particular corruption level
 stats = {"accuracy": [], "uncertainty": [], "corruption_level": list(corruption_amounts)}
@@ -50,5 +51,6 @@ for i in corruption_amounts:
     print("Corruption: {}, Acc: {}, Uncertainty: {}, Uncertainty Var: {}".format(i, acc, u, u_var))
 
 print(stats)
+
 
 #TODO plot corruption level vs. uncertainty, accuracy to visualize whether uncertainty is correlated with accuracy

train_classifier.py
@@ -18,7 +18,7 @@ checkpoint_path = os.path.join("save", dataset, "cp-{iteration:04d}.h5")
 
 # training loop
 batch_size = 64
-for i in range(30000):
+for i in range(100000):
     idx = np.random.choice(x_train.shape[0], batch_size)
     x_input_batch = tf.gather(x_train, idx)
 
@@ -32,5 +32,13 @@ for i in range(30000):
     if i % 50 == 0:
         print("[{}/30000]: {}".format(i, loss.numpy().mean()))
 
-    if i % 5000 == 0:
-        model.save(checkpoint_path.format(iteration=i))
+    if i % 1000 == 0:
+       outputs  = model(x_test)
+       alpha, probs = tf.split(outputs, 2, axis=-1)
+       y_pred = tf.argmax(probs, axis=1)
+       y_true = tf.argmax(y_test, axis=1)
+       acc = np.mean(y_pred==y_true)
+       print("Test Accuracy [{}/30000]: {}".format(i, acc))
+
+    if i % 10000 == 0:
+       model.save(checkpoint_path.format(iteration=i))